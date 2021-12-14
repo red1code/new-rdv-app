88748ab839896890a1985ac518260b0848815732
Redouane Bekkouche <redouane.bekk@gmail.com>
2021-12-14 12:21:42 +0100

    deleteing all pages and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,9 @@
-import { NotFounedComponent } from './components/not-founed/not-founed.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '', component: LandingPageComponent },
-  {
-    path: 'auth',
-    loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
-  }, {
-    path: 'home',
-    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
-  }, {
-    path: 'dashboard',
-    loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
-  { path: '**', component: NotFounedComponent },
+  // { path: '**', component: NotFounedComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NotFounedComponent } from './components/not-founed/not-founed.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NotFounedComponent,
-    LandingPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
