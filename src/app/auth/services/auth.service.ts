353cd4a55735decca659e0a7bf17d63bcb220125
Redouane Bekkouche <redouane.bekk@gmail.com>
2021-12-16 10:50:17 +0100

    setting up signup component

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,35 @@
+import { User } from './../../models/user';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private auth: AngularFireAuth,
+    private fireStore: AngularFirestore
+  ) { }
+
+  async createNewUser(user: User, userPassword: string): Promise<any> {
+    return await this.auth.createUserWithEmailAndPassword(user.email, userPassword)
+      .then((result: any) => {
+        result.user.sendEmailVerification();
+        user.role = 'subscriber';
+        user.uid = result.user.uid;
+        user.created_at = new Date();
+        user.imageURL = 'assets/unknown-profile-picture.png';
+        this.fireStore.doc('/profiles/' + user.uid).set(user)
+      }).catch((error): any => {
+        console.log('Auth Service: signup error', error);
+        if (error.code)
+          return {
+            isValid: false,
+            message: error.message,
+            code: error.code
+          };
+      });
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  isFormSubmitted: boolean = false;
+  firebaseErrorMessage!: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.signupForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.pattern(/.*\S.*/)]],
+      lastName: ['', [Validators.required, Validators.pattern(/.*\S.*/)]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmitForm() {
+    this.isFormSubmitted = true;
+
+    if (this.signupForm.invalid) return;
+
+    let password = this.signupForm.controls['password'].value;
+    let formValues = this.signupForm.value;
+    delete formValues.password;
+
+    this.authService.createNewUser(formValues, password).then((result) => {
+      if (result == null) this.router.navigate(['/home']);   // null is success, false means there was an error.
+      else if (result.isValid == false) this.firebaseErrorMessage = result.message;
+      this.isFormSubmitted = false;
+    })
   }
 
 }
