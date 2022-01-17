f4c78b50bc0e12f83c39fcf4ee3bb6b2481e6a87
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-01-17 21:37:55 +0100

    almost ready

--- src/app/pages/rendezvous/my-rendezvous/my-rendezvous.component.ts
+++ src/app/pages/rendezvous/my-rendezvous/my-rendezvous.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { Rendezvous } from 'src/app/models/rendezvous';
+import { TablesCols } from 'src/app/models/tablesCols';
+import { User } from 'src/app/models/user';
+import { RendezvousService } from 'src/app/services/rendezvous.service';
 
 @Component({
   selector: 'app-my-rendezvous',
@@ -7,9 +13,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyRendezvousComponent implements OnInit {
 
-  constructor() { }
+
+  user!: User;
+  myRDVs!: Observable<Rendezvous[]>;
+  rdvCol: TablesCols[] = [
+    { title: 'Order', data: 'order' },
+    { title: 'Display Name', data: 'displayName' },
+    { title: 'Phone Number', data: 'phoneNumber' },
+    { title: 'Created At', data: 'created_at' },
+    { title: 'Last Update', data: 'lastUpdate' },
+  ];
+  showForm: boolean = false;
+  rdv!: Rendezvous | null;
+  formErrorMsg!: string;
+
+  constructor(
+    private authService: AuthService,
+    private rdvService: RendezvousService
+  ) { }
 
   ngOnInit(): void {
+    this.authService.getUser().subscribe(usr => this.user = usr as User);
+    this.myRDVs = this.rdvService.getMyRDVs()
+  }
+
+  proceedToUpdate(data: Rendezvous) {
+    this.rdv = data;
+    this.showForm = true;
+  }
+
+  async submitRDVform(data: any) {
+    const formValues = data;
+    try {
+      await this.rdvService.updateRDV(this.rdv?.rdvID as string, formValues);
+      this.hidePopupForm()
+    }
+    catch (error) {
+      this.formErrorMsg = error as string
+    }
+  }
+
+  async deleteRDV(id: string) {
+    if (confirm('Are you sure You want to delete this Rendezvous?')) {
+      try {
+        await this.rdvService.eraseRDV(id);
+        this.hidePopupForm();
+      } catch (error) {
+        this.formErrorMsg = error as string
+      }
+    }
+  }
+
+  showPopupForm = () => this.showForm = true;
+
+  hidePopupForm() {
+    this.showForm = false;
+    this.rdv = null;
   }
 
 }
--- src/app/services/rendezvous.service.ts
+++ src/app/services/rendezvous.service.ts
@@ -2,13 +2,21 @@ import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firest
 import { Rendezvous } from './../models/rendezvous';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { AuthService } from '../auth/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RendezvousService {
 
-  constructor(private fireStore: AngularFirestore) { }
+  usrEmail!: string | undefined;
+
+  constructor(
+    private authService: AuthService,
+    private fireStore: AngularFirestore
+  ) {
+    this.authService.getUser().subscribe(usr => this.usrEmail = usr?.email)
+  }
 
   creatNewRDV(rdv: Rendezvous): Promise<DocumentReference<Rendezvous>> {
     return this.fireStore.collection<Rendezvous>('Rendezvous').add(rdv)
@@ -23,24 +31,29 @@ export class RendezvousService {
   }
 
   getRDVs(): Observable<Rendezvous[]> {
-    return  this.fireStore
-    .collection<Rendezvous>('Rendezvous', ref => ref.orderBy('created_at'))
-    .snapshotChanges()
-    .pipe(
-      map(action => {
-        let i = 1;
-        return action.map(rdv => {
-          let load = rdv.payload.doc.data();
-          return {
-            ...load,
-            rdvID: rdv.payload.doc.id,
-            created_at: load.created_at.toDate().toLocaleString(),
-            lastUpdate: load.lastUpdate ? load.lastUpdate.toDate().toLocaleString() :
-            'Not updated',
-            order: i++
-          }
+    return this.fireStore
+      .collection<Rendezvous>('Rendezvous', ref => ref.orderBy('created_at'))
+      .snapshotChanges()
+      .pipe(
+        map(action => {
+          let i = 1;
+          return action.map(rdv => {
+            let load = rdv.payload.doc.data();
+            return {
+              ...load,
+              rdvID: rdv.payload.doc.id,
+              created_at: load.created_at.toDate().toLocaleString(),
+              lastUpdate: load.lastUpdate ? load.lastUpdate.toDate().toLocaleString() :
+                'Not updated',
+              order: i++
+            }
+          })
         })
-      }));
-    }
-
+      )
   }
+
+  getMyRDVs() {
+    return this.getRDVs().pipe(map(action => action.filter(rdv => rdv.created_by === this.usrEmail)))
+  }
+
+}
