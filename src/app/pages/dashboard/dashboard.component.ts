1a55aa4d912bd954d00ed2f5bcf8ae9b96567476
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-01-22 13:14:29 +0100

    dashboard UI

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  user!: Observable<User>;
+  usersON = true;
+  RDVsON = false;
+
+  constructor(
+    private authService: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    this.user = this.authService.getUser() as Observable<User>;
+  }
+
+  showUsers() {
+    this.RDVsON = false;
+    this.usersON = true
+  }
+
+  showRDVs() {
+    this.usersON = false;
+    this.RDVsON = true
   }
 
 }
