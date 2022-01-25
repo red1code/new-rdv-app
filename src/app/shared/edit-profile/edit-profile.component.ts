de2862f960d5d877861afb86a2c57b57ff030c79
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-01-25 11:29:44 +0100

    optimize EditProfileComponent

--- src/app/shared/edit-profile/edit-profile.component.ts
+++ src/app/shared/edit-profile/edit-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { ROLES, User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-edit-profile',
@@ -9,25 +10,32 @@ import { User } from 'src/app/models/user';
 })
 export class EditProfileComponent implements OnInit {
 
-  @Input() profileInfos!: User;
+  @Input() profileInfos!: User | null;
+  @Input() currentUser!: User;
+  roles = ROLES;
+  editProfileForm!: FormGroup;
   @Output() updatedValues = new EventEmitter();
 
-  editProfileForm!: FormGroup;
-
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
     this.editProfileForm = this.formBuilder.group({
-      firstName: [this.profileInfos.firstName, [Validators.required, Validators.pattern(/.*\S.*/)]],
-      lastName: [this.profileInfos.lastName, [Validators.required, Validators.pattern(/.*\S.*/)]],
-      phoneNumber: [this.profileInfos.phoneNumber, [Validators.required, Validators.pattern(/^(\+213|0)?[0-9]{9}$/)]]
+      firstName: [this.profileInfos?.firstName, [Validators.required, Validators.pattern(/.*\S.*/)]],
+      lastName: [this.profileInfos?.lastName, [Validators.required, Validators.pattern(/.*\S.*/)]],
+      phoneNumber: [this.profileInfos?.phoneNumber, [Validators.required, Validators.pattern(/^(\+213|0)?[0-9]{9}$/)]],
+      role: [this.profileInfos?.role, Validators.required]
     })
   }
 
   submitForm() {
     this.updatedValues.emit(this.editProfileForm.value)
+  }
+
+  get isAdmin() {
+    return this.authService.canCRUDusers(this.currentUser)
   }
 
 }
