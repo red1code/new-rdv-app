e0c85ff715123455c20b721dc99edeec02606f2a
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-01-29 20:11:49 +0100

    implementing get months method in utilities

--- src/app/utils/utilities.ts
+++ src/app/utils/utilities.ts
@@ -1,3 +1,21 @@
 import { FirebaseError } from 'firebase/app';
 
 export const isFirebaseError = (x: any): x is FirebaseError => typeof x.message === 'string';
+
+export const Months: string[] = Array.from({ length: 12 }, (item, i) =>
+  new Date(0, i).toLocaleString('en', { month: 'short' })
+);
+
+
+
+
+
+
+
+/*
+
+export const Months: string[] = Array.from({ length: 12 }, (item, i) =>
+  new Date(new Date().getFullYear()-1, i).toLocaleString('en', { year: 'numeric', month: 'short' })
+);
+
+*/
