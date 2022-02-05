1c565544efb7e79d1e6400ee4bd14af9b2e0d3f9
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-02-05 12:32:55 +0100

    implementing dashboard guard

--- src/app/pages/dashboard/services/dashboard.guard.ts
+++ src/app/pages/dashboard/services/dashboard.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { User } from 'src/app/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.CheckAccessPermissions();
   }
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
@@ -19,6 +24,20 @@ export class DashboardGuard implements CanActivate, CanActivateChild, CanLoad {
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.CheckAccessPermissions();
   }
+
+  CheckAccessPermissions(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return new Promise((resolve, reject) => {
+      this.authService.getUser().subscribe(value => {
+        const user = value as User;
+        if (this.authService.canAccessDashboard(user)) resolve(true)
+        else {
+          this.router.navigate(['']);
+          reject()
+        }
+      })
+    })
+  }
+
 }
