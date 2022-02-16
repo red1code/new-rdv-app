a507200a8956346e80e469327968d3868b304517
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-02-16 18:53:38 +0100

    creating methods in the service

--- src/app/services/rendezvous.service.ts
+++ src/app/services/rendezvous.service.ts
@@ -1,5 +1,5 @@
 import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firestore';
-import { Rendezvous } from './../models/rendezvous';
+import { ApprovedRendezvous, Rendezvous } from './../models/rendezvous';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { User } from '../models/user';
@@ -8,7 +8,6 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class RendezvousService {
-
 
   constructor(private fireStore: AngularFirestore) { }
 
@@ -66,6 +65,86 @@ export class RendezvousService {
       }))
   }
 
+  // approveRDVs methods
+  async addApprovedRDV(rdv: Rendezvous, rdvDate: string, currentUser: User): Promise<void | string> {
+    const approvedRDV = this.getApprovedRDVobject(rdv, rdvDate, currentUser);
+    try {
+      await this.fireStore.collection<ApprovedRendezvous>('approved-rendezvous').add(approvedRDV);
+      await this.fireStore.collection<Rendezvous>("Rendezvous").doc(rdv.rdvID).delete()
+    }
+    catch (error) {
+      return error as string
+    }
+  }
+
+  updateApprovedRDV(id: string, rdv: ApprovedRendezvous): Promise<void> {
+    rdv.created_at = new Date(rdv.created_at);
+    rdv.approvedAt = new Date(rdv.approvedAt);
+    rdv.lastUpdate = new Date();
+    return this.fireStore.collection<ApprovedRendezvous>('approved-rendezvous').doc(id).update(rdv)
+  }
+
+  deleteApprovedRDV(id: string): Promise<void> {
+    return this.fireStore.collection<ApprovedRendezvous>('approved-rendezvous').doc(id).delete();
+  }
+
+  getApprovedRDVs(): Observable<ApprovedRendezvous[]> {
+    return this.fireStore
+      .collection<ApprovedRendezvous>('approved-rendezvous', ref => ref.orderBy('rdvDate'))
+      .snapshotChanges()
+      .pipe(map(results => {
+        let i = 1;
+        return results.map(rdv => {
+          const load = rdv.payload.doc.data();
+          return {
+            ...load,
+            rdvID: rdv.payload.doc.id,
+            rdvDate: this.convertToDateString(load.rdvDate),
+            approvedAt: this.convertToDateString(load.approvedAt),
+            created_at: this.convertToDateString(load.created_at),
+            lastUpdate: (load.lastUpdate === 'Not Updated') ? load.lastUpdate : this.convertToDateString(load.lastUpdate),
+            order: i++
+          }
+        })
+      }))
+  }
+
+  getApprovedRDVsByEmail(usrEmail: string): Observable<ApprovedRendezvous[]> {
+    return this.fireStore
+      .collection<ApprovedRendezvous>(
+        'approved-rendezvous',
+        ref => ref.where('created_by', '==', usrEmail).orderBy('rdvDate')
+      ).snapshotChanges().pipe(map(values => {
+        let i = 1;
+        return values.map(rdv => {
+          const load = rdv.payload.doc.data();
+          return {
+            ...load,
+            rdvID: rdv.payload.doc.id,
+            rdvDate: this.convertToDateString(load.rdvDate),
+            approvedAt: this.convertToDateString(load.approvedAt),
+            created_at: this.convertToDateString(load.created_at),
+            lastUpdate: (load.lastUpdate === 'Not Updated') ? load.lastUpdate : this.convertToDateString(load.lastUpdate),
+            order: i++
+          }
+        })
+      }))
+  }
+
+  private getApprovedRDVobject(rdv: Rendezvous, rdvDt: string, currentUser: User): ApprovedRendezvous {
+    return {
+      displayName: rdv.displayName,
+      phoneNumber: rdv.phoneNumber,
+      created_by: rdv.created_by,
+      created_at: new Date(rdv.created_at),
+      lastUpdate: (rdv.lastUpdate === 'Not Updated') ? rdv.lastUpdate : new Date(rdv.lastUpdate),
+      approvedBy: currentUser.email,
+      approvedAt: new Date(),
+      rdvDate: new Date(rdvDt)
+    }
+  }
+  // End of approveRDVs methods.
+
   private convertToDateString(param: any): string {
     return param.toDate().toLocaleString('en-US', {
       year: 'numeric',
@@ -73,7 +152,7 @@ export class RendezvousService {
       day: 'numeric',
       hour: '2-digit',
       minute: '2-digit',
-      hour12: false
+      // hour12: false
     })
   }
 
