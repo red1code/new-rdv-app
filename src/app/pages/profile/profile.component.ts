14889c4e922e933a2a00e5f3408ab1b2d068cceb
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-02-18 10:57:05 +0100

    Crop img (#31)
    implementing img crop feature in ProfileComponent

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { ActivatedRoute } from '@angular/router';
+import { ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
 import { Observable, finalize, of } from 'rxjs';
 import { User } from 'src/app/models/user';
 import { UsersService } from 'src/app/services/users.service';
+import { dataUrlToFile } from 'src/app/utils/utilities';
 
 @Component({
   selector: 'app-profile',
@@ -13,10 +15,13 @@ import { UsersService } from 'src/app/services/users.service';
 export class ProfileComponent implements OnInit {
 
   user!: User;
+  errorMsg = '';
+
   editProfilePopup = false;
-  updateErrMsg = '';
 
   editPicPopup = false;
+  imageFile: LoadedImage | null = null;
+  croppedImage!: string | null;
   uploading = false;
   percentage!: Observable<number | undefined>;
 
@@ -28,9 +33,7 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit(): void {
     const uid = this.route.snapshot.params['id'];
-    this.usersService.getCurrentProfile(uid).subscribe(value => {
-      this.user = value as User;
-    })
+    this.usersService.getCurrentProfile(uid).subscribe(value => this.user = value as User)
   }
 
   async updateProfile(data: User) {
@@ -39,42 +42,67 @@ export class ProfileComponent implements OnInit {
       this.hideEditProfilePopUp();
       this.hideEditPicPopup();
     } catch (error) {
-      this.updateErrMsg = error as string;
+      this.errorMsg = error as string;
     }
   }
 
-  uploadPhoto(event: any) {
+  showEditProfilePopUp = () => this.editProfilePopup = true;
+
+  hideEditProfilePopUp() {
+    this.editProfilePopup = false;
+    this.errorMsg = '';
+  }
+
+  hideEditPicPopup() {
+    this.editPicPopup = false;
+    this.uploading = false;
+    this.errorMsg = '';
+    this.percentage = of(undefined);
+    this.imageFile = null;
+    this.croppedImage = null;
+  }
+
+  // crop image
+  fileChangeEvent(event: any): void {
+    if (event.target.files[0]) {
+      this.imageFile = event;
+      this.errorMsg = ''
+      this.editPicPopup = true;
+    }
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64 as string;
+  }
+
+  loadImageFailed() {
+    this.imageFile = null;
+    this.errorMsg = 'Please, Load a valid image'
+    setTimeout(() => { this.hideEditPicPopup() }, 3000)
+  }
+
+  saveImg() {
+    this.uploadPhoto(this.croppedImage as string)
+  }
+
+  async uploadPhoto(event: string) {
+    if (!event) return;
     this.uploading = true;
+    const image = await dataUrlToFile(event);
     const filePath = `profile-pictures/${this.user.uid}`;
     const fileRef = this.afStorage.ref(filePath);
-    const task = this.afStorage.upload(filePath, event.target.files[0]);
+    const task = this.afStorage.upload(filePath, image);
     this.percentage = task.percentageChanges();
     task.snapshotChanges().pipe(
       finalize(() => {
         fileRef.getDownloadURL().subscribe(url => {
           if (url) {
             this.user.imageURL = url;
-            this.updateProfile(this.user).then(() => this.uploading = false)
+            this.updateProfile(this.user).then(() => this.hideEditPicPopup())
           }
         })
       })
-    ).subscribe()
-  }
-
-  showEditProfilePopUp = () => this.editProfilePopup = true;
-
-  showEditPicPopup = () => this.editPicPopup = true;
-
-  hideEditProfilePopUp() {
-    this.editProfilePopup = false;
-    this.updateErrMsg = '';
-  }
-
-  hideEditPicPopup() {
-    this.editPicPopup = false;
-    this.uploading = false;
-    this.updateErrMsg = '';
-    this.percentage = of(undefined)
+    ).subscribe();
   }
 
 }
