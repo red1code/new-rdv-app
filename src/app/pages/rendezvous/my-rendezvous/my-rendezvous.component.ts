4712fb2d944c9f1a107dd88b44a2f9ce38b424b2
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-02-21 11:54:00 +0100

    updating My-Rendezvous page

--- src/app/pages/rendezvous/my-rendezvous/my-rendezvous.component.ts
+++ src/app/pages/rendezvous/my-rendezvous/my-rendezvous.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/auth/services/auth.service';
-import { Rendezvous } from 'src/app/models/rendezvous';
+import { Rendezvous, RendezvousStates } from 'src/app/models/rendezvous';
 import { TablesCols } from 'src/app/models/tablesCols';
 import { User } from 'src/app/models/user';
 import { RendezvousService } from 'src/app/services/rendezvous.service';
@@ -13,19 +13,28 @@ import { RendezvousService } from 'src/app/services/rendezvous.service';
 })
 export class MyRendezvousComponent implements OnInit {
 
-
   user!: User;
-  myRDVs!: Observable<Rendezvous[]>;
-  rdvCol: TablesCols[] = [
+  myApprovedRDVs!: Observable<Rendezvous[]>;
+  myApprovedRDVsCols: TablesCols[] = [
     { title: 'Order', data: 'order' },
     { title: 'Display Name', data: 'displayName' },
     { title: 'Phone Number', data: 'phoneNumber' },
-    { title: 'Created At', data: 'created_at' },
+    { title: 'Created At', data: 'createdAt' },
+    { title: 'Last Update', data: 'lastUpdate' },
+    { title: 'Rendezvous Date', data: 'rdvDate' }
+  ];
+  myPendingRDVs!: Observable<Rendezvous[]>;
+  myPendingRDVsCols: TablesCols[] = [
+    { title: 'Order', data: 'order' },
+    { title: 'Display Name', data: 'displayName' },
+    { title: 'Phone Number', data: 'phoneNumber' },
+    { title: 'Created At', data: 'createdAt' },
     { title: 'Last Update', data: 'lastUpdate' },
   ];
-  showForm: boolean = false;
+  halfwayPopup = false;
+  updatePopup = false;
   rdv!: Rendezvous | null;
-  formErrorMsg!: string;
+  errMsg = '';
 
   constructor(
     private authService: AuthService,
@@ -35,40 +44,79 @@ export class MyRendezvousComponent implements OnInit {
   ngOnInit(): void {
     this.authService.getUser().subscribe(usr => {
       this.user = usr as User;
-      // this.myRDVs = this.rdvService.getRDVsByEmail(this.user.email)
+
+      this.myApprovedRDVs = this.rdvService.getApprovedRendezvousByEmail(this.user.email);
+      this.myPendingRDVs = this.rdvService.getPendingRendezvousByEmail(this.user.email)
     })
   }
 
-  proceedToUpdate(data: Rendezvous) {
+  showHalfwayPopup(data: Rendezvous) {
     this.rdv = data;
-    this.showForm = true;
+    this.halfwayPopup = true
   }
 
-  async submitRDVform(data: any) {
-    const formValues = data;
-    // try {
-    //   await this.rdvService.updateRDV(this.rdv?.rdvID as string, formValues);
-    //   this.hidePopupForm()
-    // }
-    // catch (error) {
-    //   this.formErrorMsg = error as string
-    // }
+  proceedToUpdate() {
+    this.halfwayPopup = false;
+    this.updatePopup = true
   }
 
-  async deleteRDV(id: string) {
-    // if (confirm('Are you sure You want to delete this Rendezvous?')) {
-    //   try {
-    //     await this.rdvService.eraseRDV(id);
-    //     this.hidePopupForm();
-    //   } catch (error) {
-    //     this.formErrorMsg = error as string
-    //   }
-    // }
+  get isPending() {
+    return (this.rdv?.rdvState === RendezvousStates.PENDING) ? true : false
   }
 
-  hidePopupForm() {
-    this.showForm = false;
+  get isApproved() {
+    return (this.rdv?.rdvState === RendezvousStates.APPROVED) ? true : false
+  }
+
+  async updateRendezvous(data: Rendezvous) {
+    if (!this.rdv?.rdvID) return this.errMsg = 'Rendezvous not found';
+    if (data.displayName === this.rdv.displayName && data.phoneNumber === this.rdv.phoneNumber)
+      return this.hidePopup(); // no change was made.
+    try {
+      await this.rdvService.updateRendezvous(this.rdv.rdvID, data);
+      return this.hidePopup()
+    }
+    catch (error) {
+      this.errMsg = error as string
+    }
+  }
+
+  async deleteRendezvous() {
+    if (!this.rdv?.rdvID) return this.errMsg = 'Rendezvous ID not found';
+    if (confirm(`Are you sure You want to delete this Rendezvous? \n
+      - Name: ${this.rdv?.displayName} \n
+      - Created At: ${this.rdv?.createdAt}`
+    )) {
+      try {
+        await this.rdvService.deleteRendezvous(this.rdv.rdvID, this.rdv, this.user);
+        return this.hidePopup()
+      } catch (error) {
+        this.errMsg = error as string
+      }
+    }
+  }
+
+  async cancelRendezvous() {
+    if (!this.rdv?.rdvID) return this.errMsg = 'Rendezvous ID not found';
+    if (confirm(`Are you sure You want to Call Off this Rendezvous? \n
+      - Name: ${this.rdv?.displayName} \n
+      - Created At: ${this.rdv?.createdAt}`
+    )) {
+      this.rdv.rdvState = RendezvousStates.CANCELED;
+      try {
+        await this.rdvService.cancelRendezvous(this.rdv.rdvID, this.rdv, this.user);
+        return this.hidePopup()
+      } catch (error) {
+        this.errMsg = error as string
+      }
+    }
+  }
+
+  hidePopup() {
+    this.halfwayPopup = false;
+    this.updatePopup = false;
     this.rdv = null;
+    this.errMsg = ''
   }
 
 }
