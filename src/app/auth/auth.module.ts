99995ffbdff1e883262ce0f32471d6818c7b26ca
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-03-02 19:51:22 +0100

    translating templates

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
