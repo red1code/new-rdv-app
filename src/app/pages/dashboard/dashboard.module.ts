f6d5f18c2cb2042741b8416dbe2b49f7b4a7414d
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-03-09 19:00:33 +0100

    adding Medecin role feature (#32)
    implementing Medecin role

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { UsersPartComponent } from './users-part/users-part.component';
-import { RdvsPartComponent } from './rdvs-part/rdvs-part.component';
+import { RendezvousComponent } from './rendezvous/rendezvous.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
 
 @NgModule({
   declarations: [
     DashboardComponent,
     UsersPartComponent,
-    RdvsPartComponent
+    RendezvousComponent,
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule
   ]
 })
 export class DashboardModule { }
--- src/app/utils/utilities.ts
+++ src/app/utils/utilities.ts
@@ -18,6 +18,14 @@ export async function dataUrlToFile(dataUrl: string): Promise<File> {
 
 
 
+export function timeTolocalISOstring(date: Date): string {
+  return (date.getFullYear().toString() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + (date.getDate())).slice(-2)) + 'T' + date.toTimeString().slice(0, 5)
+}
+
+
+
+
+
 /*
 
 export const Months: string[] = Array.from({ length: 12 }, (item, i) =>
