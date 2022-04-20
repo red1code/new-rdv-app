fd863951b5b0d1a412cb37ceb4b94df6785e68d0
Redouane Bekkouche <redouane.bekk@gmail.com>
2022-04-20 11:28:38 +0100

    dark mode setup

--- src/app/utils/utilities.ts
+++ src/app/utils/utilities.ts
@@ -1,4 +1,5 @@
 import { FirebaseError } from 'firebase/app';
+import { THEMES } from '../models/user';
 
 export const isFirebaseError = (x: any): x is FirebaseError => typeof x.message === 'string';
 
@@ -10,6 +11,27 @@ export async function dataUrlToFile(dataUrl: string): Promise<File> {
 
 export function timeTolocalISOstring(date: Date): string {
   return (date.getFullYear().toString() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + (date.getDate())).slice(-2)) + 'T' + date.toTimeString().slice(0, 5)
+}
+
+export function setTheme(theme: THEMES) {
+  const root = <HTMLElement>document.querySelector(':root');
+  if (theme === THEMES.DARK) {
+    root.style.setProperty('--font-color', 'rgba(100%, 100%, 100%, 87%)');
+    root.style.setProperty('--header-font', 'rgba(100%, 100%, 100%, 87%)');
+    root.style.setProperty('--template-bg-color', '#121212');
+    root.style.setProperty('--popup-bg-color', '#000000');
+    root.style.setProperty('--body-bg-color', '#000000');
+    root.style.setProperty('--mainToSide-color', '#ecf39e');
+    root.style.setProperty('--overlay-color', '#ffffff');
+  } else { // set light
+    root.style.setProperty('--font-color', '#121212');
+    root.style.setProperty('--header-font', '#ffffff');
+    root.style.setProperty('--template-bg-color', '#ffffff');
+    root.style.setProperty('--popup-bg-color', '#ffffff');
+    root.style.setProperty('--body-bg-color', '#ebebeb');
+    root.style.setProperty('--mainToSide-color', '#191970');
+    root.style.setProperty('--overlay-color', '#000000');
+  }
 }
 
 
